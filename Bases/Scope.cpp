ff806ae47592487afac574436a613ff1d834c5f7
Elizaveta Demina <liza_demina11@mail.ru>
2021-04-07 00:33:59 +0300

    Merge

--- Bases/Scope.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by deliza on 04.04.2021.
-//
-
-#include "Scope.h"
-
-void Scope::AddVariable(Variable *var) {
-    variables_.push_back(var);
-}
--- Bases/Variable.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by deliza on 04.04.2021.
-//
-
-#include "Variable.h"
-
-Variable::Variable(const std::string &name, Class *class_ = nullptr): name_(name), class_(class_) {
-
-}
